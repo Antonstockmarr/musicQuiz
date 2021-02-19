dfb66f8ba330df6f07ec7720383bf3fba27a38a9
sorenhartmann <soren.hartmann@gmail.com>
2021-02-19 22:56:16 +0100

    vi prøver anton

---
+++ src/static/index.js
@@ -0,0 +1,12 @@
+const vm = new Vue({ // Again, vm is our Vue instance's name for consistency.
+    el: '#vm',
+    delimiters: ['[[', ']]']
+})
+
+const vm = new Vue({ // Again, vm is our Vue instance's name for consistency.
+    el: '#vm',
+    delimiters: ['[[', ']]'],
+    data: {
+        greeting: 'Hello, Vue!'
+    }
+})
--- src/webapp.py
+++ src/webapp.py
@@ -1,7 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
-    return "Her skal være musikfuck!"
+    return render_template("index.html")
+
